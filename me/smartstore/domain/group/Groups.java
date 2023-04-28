84c8897e2c1fab67044615cfdc163d2ac13cc519
khsrla9806 <kimhunsope@naver.com>
2023-04-29 00:00:49 +0900

    Feat: Groups 인스턴스 초기 생성시 Group 4개 미리 인스턴스화
    - 원하는 인덱스에 그룹 추가할 때 NullPointerException을 피하기 위함
    - Group은 NONE, GENERAL, VIP, VVIP 4개만 만들어져서 관리되기 때문에 이렇게 설정했습니다.

--- me/smartstore/domain/group/Groups.java
+++ me/smartstore/domain/group/Groups.java
@@ -8,6 +8,8 @@ public class Groups extends MyArray<Group> {
     public static Groups getInstance() {
         if (allGroups == null) {
             allGroups = new Groups();
+            for (int i = 0; i < 4; i++) allGroups.add(new Group()); // NONE, GENERAL, VIP, VVIP (4개 생성)
+            allGroups.set(0, new Group(new Parameter(0, 0), GroupType.NONE)); // NONE은 초기에 생성
         }
         return allGroups;
     }

19610340d99d56f2c4bf9c44d11c3307010eea3e
khsrla9806 <kimhunsope@naver.com>
2023-04-28 19:09:27 +0900

    Feat: Group을 저장할 동적 배열인 Groups 클래스를 생성
    - Repository 개념으로 싱글톤으로 만들었습니다.

---
+++ me/smartstore/domain/group/Groups.java
@@ -0,0 +1,16 @@
+package me.smartstore.domain.group;
+
+import me.smartstore.utils.MyArray;
+
+public class Groups extends MyArray<Group> {
+    private static Groups allGroups;
+
+    public static Groups getInstance() {
+        if (allGroups == null) {
+            allGroups = new Groups();
+        }
+        return allGroups;
+    }
+
+    private Groups() {}
+}

fa90e19d928c57c9afb02ecfad3f334565a80114
khsrla9806 <kimhunsope@naver.com>
2023-04-29 01:24:01 +0900

    Feat: Customer와 Parameter가 추가, 수정될 때 고객 데이터 분류를 갱신해줄 refresh() 메서드 구현

--- me/smartstore/domain/customer/Customers.java
+++ me/smartstore/domain/customer/Customers.java
@@ -1,9 +1,15 @@
 package me.smartstore.domain.customer;
 
+import me.smartstore.domain.group.Group;
+import me.smartstore.domain.group.GroupType;
+import me.smartstore.domain.group.Groups;
 import me.smartstore.utils.MyArray;
+
+import static me.smartstore.domain.group.GroupType.getTypeLevel;
 
 public class Customers extends MyArray<Customer> {
     private static Customers allCustomers;
+    private final Groups allGroups;
 
     public static Customers getInstance() {
         if (allCustomers == null) {
@@ -12,8 +18,32 @@ public class Customers extends MyArray<Customer> {
         return allCustomers;
     }
 
-    private Customers() {}
+    private Customers() {
+        this.allGroups = Groups.getInstance();
+    }
 
     // TODO: Refresh 메서드 구현 (새로운 고객 추가, 분류기준 변경시 작동)
-    public void refresh() {}
+    public void refresh() {
+        for (int i = 0; i < allCustomers.size(); i++) {
+            Customer customer = allCustomers.get(i);
+            Group group = findGroupOfCustomer(customer.getTotalUsageTime(), customer.getTotalPurchaseAmount());
+            customer.setGroup(group);
+        }
+    }
+
+    public Group findGroupOfCustomer(int usageTime, int purchaseAmount) {
+        Group group = allGroups.get(0); // 처음에는 NONE
+        for (int i = 1; i < allGroups.size(); i++) {
+            Group pickedGroup = allGroups.get(i);
+            int minUsageTime = pickedGroup.getParameter().getMinUsageTime();
+            int minPurchase = pickedGroup.getParameter().getMinPurchaseAmount();
+
+            if (usageTime >= minUsageTime && purchaseAmount >= minPurchase) {
+                if (getTypeLevel(group) < getTypeLevel(pickedGroup)) {
+                    group = pickedGroup;
+                }
+            }
+        }
+        return group;
+    }
 }

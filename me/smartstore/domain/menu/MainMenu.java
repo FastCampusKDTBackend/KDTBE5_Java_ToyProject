387749e083719903c19d6ddbcb2084f68832fabd
khsrla9806 <kimhunsope@naver.com>
2023-04-28 18:22:16 +0900

    Feat: Customer, Group, Summary 메뉴에 대한 분기를 가지고 있는 MainMenu 클래스 생성

---
+++ me/smartstore/domain/menu/MainMenu.java
@@ -0,0 +1,45 @@
+package me.smartstore.domain.menu;
+
+import me.smartstore.SmartStoreApplication;
+
+public class MainMenu implements Menu {
+    private final CustomerMenu customerMenu;
+    private final GroupMenu groupMenu;
+    private final SummaryMenu summaryMenu;
+
+    private static MainMenu mainMenu;
+
+    public static MainMenu getInstance() {
+        if (mainMenu == null) {
+            mainMenu = new MainMenu();
+        }
+        return mainMenu;
+    }
+    private MainMenu() {
+        this.customerMenu = CustomerMenu.getInstance();
+        this.groupMenu = GroupMenu.getInstance();
+        this.summaryMenu = SummaryMenu.getInstance();
+    }
+
+    @Override
+    public void manage() {
+        while ( true ) { // 프로그램 실행 while
+            int choice = mainMenu.chooseMenu(new String[] {
+                    "Parameter",
+                    "Customer",
+                    "Classification Summary",
+                    "Quit"});
+
+            if (choice == 1) {
+                groupMenu.manage();
+            } else if (choice == 2) {
+                customerMenu.manage();
+            } else if (choice == 3) {
+                summaryMenu.manage();
+            } else {
+                System.out.println("Program Finished");
+                break;
+            }
+        }
+    }
+}

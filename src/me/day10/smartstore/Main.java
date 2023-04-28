2bfe7f6326e23c1e60d12d125947d06a4fa92489
Joonhee Jeong <jeonggoo75@gmail.com>
2023-04-28 21:56:53 +0900

    feat: main method

---
+++ src/me/day10/smartstore/Main.java
@@ -0,0 +1,8 @@
+package me.day10.smartstore;
+
+public class Main {
+    public static void main(String[] args) {
+//        SmartStoreApp.test().run();
+        SmartStoreApp.getInstance().run();
+    }
+}

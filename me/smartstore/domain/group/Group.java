0d1e613bb5238bb725be103ec3bd6e47b94acc83
khsrla9806 <kimhunsope@naver.com>
2023-04-28 19:07:01 +0900

    Feat: 고객을 분류해서 저장할 Group 클래스 생성
    - Parameter: 해당 그룹의 분류기준
    - GroupType: 해당 그룹의 타입
    - 기본 생성자, 모든 파라미터를 가지고 있는 생성자
    - getter/setter, hashCode, equals, toString

---
+++ me/smartstore/domain/group/Group.java
@@ -0,0 +1,57 @@
+package me.smartstore.domain.group;
+
+import java.util.Objects;
+
+public class Group {
+    private Parameter parameter;
+    private GroupType groupType;
+
+    public Group() {}
+
+    public Group(Parameter parameter, GroupType groupType) {
+        this.parameter = parameter;
+        this.groupType = groupType;
+    }
+
+    public Parameter getParameter() {
+        return parameter;
+    }
+
+    public GroupType getGroupType() {
+        return groupType;
+    }
+
+    public void setParameter(Parameter parameter) {
+        this.parameter = parameter;
+    }
+
+    public void setGroupType(GroupType groupType) {
+        this.groupType = groupType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Group group = (Group) o;
+
+        if (!Objects.equals(parameter, group.parameter)) return false;
+        return groupType == group.groupType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = parameter != null ? parameter.hashCode() : 0;
+        result = 31 * result + (groupType != null ? groupType.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "parameter=" + parameter +
+                ", groupType=" + groupType +
+                '}';
+    }
+}

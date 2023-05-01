58fbf2e8d3e72c5c6e65f4d3264f7aa6ca70786d
jy-b <jongyoonb+u@gmail.com>
2023-05-02 02:32:04 +0900

    refactor: Add CustomHashMap.java
    [Add CustomHashMap.java]

---
+++ src/com/smartstore/util/CustomHashMap.java
@@ -0,0 +1,172 @@
+package com.smartstore.util;
+
+public class CustomHashMap<K,V>{
+    //Entry Class for CustomHashMap
+    private class Entry<K, V>{
+        private K key;
+        private V value;
+        private Entry<K,V> next;
+
+        public Entry(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        //Key is immutable
+        public K getKey() {
+            return this.key;
+        }
+
+        public V getValue() {
+            return this.value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Entry <K, V> e = this;
+            while (e != null){
+                sb.append("\t").append("[").append(" key :  ").append(e.getKey()).append(", value : ").append(e.getValue()).append(" ]\n");
+                e = e.next;
+            }
+            return sb.toString();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CustomHashMap { \n");
+        for(int i = 0 ; i < SIZE ; i++){
+            if(entries[i] != null){
+                sb.append(entries[i]);
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /*
+        Entries
+        +--------------------+--------------------+--------------------+--------------------+--------------------+
+        | Head1              | Head2              | Head3              |        ...         | Head.SIZE          |
+        | key.hash % SIZE    | key.hash % SIZE    | key.hash % SIZE    | key.hash % SIZE    | key.hash % SIZE    |
+        +--------------------+--------------------+--------------------+--------------------+--------------------+
+        | [key, value]       | null               | null               | [key, value]       | [key, value]       |
+        | [key, value]       |                    |                    |                    | [key, value]       |
+        | [key, value]       |                    |                    |                    | [key, value]       |
+        |                    |                    |                    |                    | [key, value]       |
+        +--------------------+--------------------+--------------------+--------------------+--------------------+
+        Too big SIZE have chance to increase unnecessary indexes
+     */
+    private final int SIZE = 5;
+
+    private Entry<K, V>[] entries;
+
+    public CustomHashMap(){
+        entries = new Entry[SIZE];
+    }
+
+    public void put(K key, V value){
+        //Get Entries index from hash code 0 to size-1
+        int hash = key.hashCode() % SIZE;
+        Entry<K, V> e = entries[hash];
+
+        //if entries[hash] is empty
+        if(e == null){
+            //create new Entry
+            entries[hash] = new Entry<K, V>(key, value);
+        }
+        else{
+            //loop until entries value not exists
+            while (e.next != null){
+                //if key exists, set value as new one
+                if (e.getKey() == key){
+                    e.setValue(value);
+                    return;
+                }
+                e = e.next;
+            }
+            //if key == final entry's key, set value
+            if(e.getKey() == key){
+                e.setValue(value);
+                return;
+            }
+
+            //if key doesn't exist
+            //add new Entry end of the entries[hash]
+            e.next = new Entry<K, V>(key, value);
+        }
+
+    }
+
+    public V get(K key){
+        int hash = key.hashCode() % SIZE;
+        Entry<K, V> e = entries[hash];
+
+        //if entries[hash] is empty, return null
+        if (e == null) {
+            return null;
+        }
+
+        //loop until entries value not exists
+        while (e != null){
+            //if key exits, return value
+            if(e.getKey() == key){
+                return e.getValue();
+            }
+            e = e.next;
+        }
+        //if key doesn't exist, return null
+        return null;
+    }
+
+    public V getOrDefault(K key, V defaultValue){
+        V value = get(key);
+
+        if(value == null){
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Entry<K, V> remove(K key){
+        int hash = key.hashCode() % SIZE;
+        Entry<K, V> e = entries[hash];
+
+        //if entries[hash] is empty, return null
+        if (e == null){
+            return null;
+        }
+
+        //if first Entry's key == key
+        if(e.getKey() == key){
+            //shift second Entry to first(remove)
+            entries[hash] = e.next;
+            e.next = null;
+            return e;
+        }
+
+        Entry<K, V> prev = e;
+        e = e.next;
+
+        while (e != null){
+            if (e.getKey() == key) {
+                //shift next Entry to current(remove)
+                prev.next = e.next;
+                e.next = null;
+                return e;
+            }
+            prev = e;
+            e = e.next;
+        }
+
+        //if key not exits, return null
+        return null;
+    }
+
+}

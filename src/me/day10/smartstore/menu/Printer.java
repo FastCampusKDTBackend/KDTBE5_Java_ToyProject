de6d2bd70e3c6432aa58ca06f737dbeddb5c0592
Joonhee Jeong <jeonggoo75@gmail.com>
2023-05-01 14:46:58 +0900

    feat: add class `Printer`

---
+++ src/me/day10/smartstore/menu/Printer.java
@@ -0,0 +1,22 @@
+package me.day10.smartstore.menu;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Printer {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static final Printer INSTANCE = new Printer();
+    private Printer() {}
+    public static Printer getInstance() { return INSTANCE; }
+
+    public void print(String str) throws RuntimeException {
+        try {
+            bw.write(str);
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

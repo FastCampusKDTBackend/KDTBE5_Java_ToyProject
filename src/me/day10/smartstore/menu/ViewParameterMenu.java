7addb20eb3ef02d9e43b0ce21aa3517268bcdca4
Joonhee Jeong <jeonggoo75@gmail.com>
2023-05-01 22:50:02 +0900

    feat: implement class `ViewParameterMenu`

--- src/me/day10/smartstore/menu/ViewParameterMenu.java
+++ src/me/day10/smartstore/menu/ViewParameterMenu.java
@@ -1,5 +1,9 @@
 package me.day10.smartstore.menu;
 
+import me.day10.smartstore.group.GroupType;
+
+import java.util.Arrays;
+
 public class ViewParameterMenu implements Menu {
 
     private static class InstanceHolder {
@@ -8,8 +12,40 @@ public class ViewParameterMenu implements Menu {
     private ViewParameterMenu() {}
     public static ViewParameterMenu getInstance() { return ViewParameterMenu.InstanceHolder.INSTANCE; }
 
+    private static final Reader reader = Reader.getInstance();
+    private static final Printer printer = Printer.getInstance();
+    private static final String GROUP_OUTPUT =
+            "Which group (GENERAL (G), VIP (V), VVIP (VV))?\n" +
+                    "** Press 'end', if you want to exit! **\n";
+
     @Override
     public Menu printAndInputAndGetNextMenu() {
-        return null;
+        while (true) {
+            print(GROUP_OUTPUT);
+            try {
+                String groupName = InputGroupName();
+                GroupType groupType = Arrays.stream(GroupType.values())
+                        .filter(t -> t.getShortcut().equals(groupName) || t.name().equals(groupName))
+                        .findAny()
+                        .orElseThrow(IllegalArgumentException::new);
+                print(groupType.toString());
+            } catch (BackMenuException e) {
+                print(e.getMessage());
+                return GroupMenu.getInstance();
+            } catch (IllegalArgumentException e) {
+                print("\nInvalid Group Name for Input. Please try again.\n");
+            }
+        }
+    }
+
+    private void print(String s) {
+        printer.print(s);
+    }
+
+    private String InputGroupName() throws BackMenuException {
+        String s = reader.inputString().toUpperCase();
+        if (s.equals("END"))
+            throw new BackMenuException("'end' is pressed. Exit this menu.\n");
+        return s;
     }
 }

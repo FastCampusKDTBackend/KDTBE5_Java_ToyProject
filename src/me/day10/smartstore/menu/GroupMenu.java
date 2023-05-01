99f584a3ece2c4952a9c983233abd396746501bf
Joonhee Jeong <jeonggoo75@gmail.com>
2023-05-01 15:43:19 +0900

    feat: implement the skeleton of `GroupMenu`

--- src/me/day10/smartstore/menu/GroupMenu.java
+++ src/me/day10/smartstore/menu/GroupMenu.java
@@ -8,8 +8,36 @@ public class GroupMenu implements Menu {
     private GroupMenu() {}
     public static GroupMenu getInstance() { return InstanceHolder.INSTANCE; }
 
+    private static final String OUTPUT =
+                    '\n' +
+                    "==========Group Menu==========" + '\n' +
+                    "1. " + "Set Parameter" + '\n' +
+                    "2. " + "View Parameter" + '\n' +
+                    "3. " + "Update Parameter" + '\n' +
+                    "4. " + "Back" + '\n' +
+                    "==============================" + '\n' +
+                    "Choose One: ";
+
+    private final Menu[] nextMenu = {
+            null,
+    };
+
     @Override
     public Menu printAndInputAndGetNextMenu() {
-        return null;
+        while (true) {
+            try {
+                print(OUTPUT);
+                int menu = inputMenu();
+                return nextMenu[menu];
+            } catch (Exception e) {
+                print(e.getMessage());
+            }
+        }
+    }
+
+    private void print(String s) {}
+
+    private int inputMenu() {
+        return 0;
     }
 }

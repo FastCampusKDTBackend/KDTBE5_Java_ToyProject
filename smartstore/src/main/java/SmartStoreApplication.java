3e63498cc416282f9bf334f08f4f4145f2b77b31
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-05-03 16:22:58 +0900

    feat: InputView, OutputView 의존성 주입 및 메뉴 입력받기 구현

--- smartstore/src/main/java/SmartStoreApplication.java
+++ smartstore/src/main/java/SmartStoreApplication.java
@@ -1,24 +1,82 @@
+import util.view.InputView;
+import util.view.OutputView;
+
 public class SmartStoreApplication {
     private static SmartStoreApplication smartStoreApplication = null;
-    private SmartStoreApplication() {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private SmartStoreApplication(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public static SmartStoreApplication getInstance() {
 
         if (smartStoreApplication == null) {
-            smartStoreApplication = new SmartStoreApplication();
+            smartStoreApplication = new SmartStoreApplication(new InputView(), new OutputView());
         }
 
         return smartStoreApplication;
     }
 
     public SmartStoreApplication test() {
-
-
         return this;
     }
 
     public void run() {
+        outputView.copyRight();
 
+        while (true) {
+            outputView.choiceMenu();
+            outputView.chooseOne();
+
+            try {
+                int menuNumber = inputView.getMenuNumber();
+                if (menuNumber == 4) {
+                    System.out.println("Program Finished");
+                    break;
+                }
+
+                if (menuNumber == 1) {
+                    executeParameterMenu();
+                }
+
+                if (menuNumber == 2) {
+                    executeCustomerDataMenu();
+                }
+
+                if (menuNumber == 3) {
+                    executeClassificationSummaryMenu();
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+        }
+
+    }
+
+    private void executeClassificationSummaryMenu() {
+    }
+
+    private void executeCustomerDataMenu() {
+    }
+
+    private void executeParameterMenu() {
+        outputView.choiceParameterMenu();
+
+    }
+
+
+    private void setParameter() {
+    }
+
+    private void viewParameter() {
+        
+    }
+
+    private void updateParameter() {
     }
 }

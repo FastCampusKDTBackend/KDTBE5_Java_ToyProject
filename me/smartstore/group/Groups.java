9fdf64bd7f6e3ba62c81a6c7de478b11cea49eee
seoyonghyun <syh05247@naver.com>
2023-05-03 21:23:29 +0900

    Feat: Groups 추가
    DArray를 상속받는 Groups는 싱글턴 객체로 이루어져 있음
    - find 함수는 매개변수로 들어오는 GroupType 타입의 변수를 for문을 이용하여 찾아내는 함수이다.

---
+++ me/smartstore/group/Groups.java
@@ -0,0 +1,25 @@
+package me.smartstore.group;
+
+import me.smartstore.arrays.DArray;
+
+public class Groups extends DArray<Group> {
+    private static Groups allGroups;
+
+    public static Groups getInstance() {
+        if (allGroups == null) {
+            allGroups = new Groups();
+        }
+        return allGroups;
+    }
+
+    private Groups() {}
+
+    public Group find(GroupType groupType) {
+        for(int i = 0; i < allGroups.size(); i++) {
+            if(allGroups.get(i).getGroupType() == groupType) {
+                return allGroups.get(i);
+            }
+        }
+        return null;
+    }
+}

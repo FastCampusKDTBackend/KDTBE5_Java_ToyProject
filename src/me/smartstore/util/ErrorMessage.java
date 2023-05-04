afd975dfeb4e6334e6f3325d9144273bc5f25c6d
HyeJiRoh <shgpwl509@gmail.com>
2023-05-04 14:14:33 +0900

    기본 ErrorMessage 클래스 생성

---
+++ src/me/smartstore/util/ErrorMessage.java
@@ -0,0 +1,22 @@
+package me.smartstore.util;
+
+public class ErrorMessage {
+
+    public static final String ERR_MSG_INVALID_ARR_EMPTY = "No Customers. Please input one first.";
+
+    public static final String ERR_MSG_NULL_ARR_ELEMENT = "Elements in Array has null. Array can't be sorted.";
+
+    public static final String ERR_MSG_INVALID_INPUT_NULL = "Null Input. Please input something.";
+
+    public static final String ERR_MSG_INVALID_INPUT_EMPTY = "Empty Input. Please input something.";
+
+    public static final String ERR_MSG_INVALID_INPUT_RANGE = "Invalid Input. Please try again.";
+
+    public static final String ERR_MSG_INVALID_INPUT_TYPE = "Invalid Type for Input. Please try again.";
+
+    public static final String ERR_MSG_INVALID_INPUT_FORMAT = "Invalid Format for Input. Please try again.";
+
+    public static final String ERR_MSG_INPUT_END = "END is pressed. Exit this menu.";
+
+    public static final String END_MSG = "END";
+}

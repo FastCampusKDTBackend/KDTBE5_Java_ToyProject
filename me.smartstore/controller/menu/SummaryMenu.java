800f5c4971dbe0dedcbb150453efc60fe7a23bf9
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-05 01:20:31 +0900

    refactor: 그룹별로 정렬된 고객리스트를 출력하는 기능 수정

--- me.smartstore/controller/menu/SummaryMenu.java
+++ me.smartstore/controller/menu/SummaryMenu.java
@@ -1,9 +1,17 @@
 package controller.menu;
 
+import domain.customer.Customer;
+import domain.group.Group;
+import exception.ArrayEmptyException;
 import exception.InputEndException;
+import service.GroupService;
 import service.SummaryService;
 import view.Input;
+import view.Output;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class SummaryMenu implements Menu {
@@ -13,7 +21,8 @@ public class SummaryMenu implements Menu {
     private static final int MENU_ITEMS_MIN_NUM;
 
     private static SummaryMenu summaryMenu;
-    private static SummaryService summaryService;
+    private static final SummaryService summaryService;
+    private static final GroupService groupService;
 
     static {
         MENU_ITEMS = new String[]{"Summary", "Summary (Sorted By Name)",
@@ -21,6 +30,7 @@ public class SummaryMenu implements Menu {
         MENU_ITEMS_MAX_NUM = MENU_ITEMS.length;
         MENU_ITEMS_MIN_NUM = 1;
         summaryService = SummaryService.getInstance();
+        groupService = GroupService.getInstance();
     }
 
     public static SummaryMenu getInstance() {
@@ -52,12 +62,33 @@ public class SummaryMenu implements Menu {
     @Override
     public void service(int menuNum) {
         try{
-            if (menuNum == 1) summaryService.summaryDefault();
+            if (menuNum == 1) printSummaryDefault();
             if (menuNum == 2) summaryService.summarySortedByName(Input.isSummarySortOrderDesc());
             if (menuNum == 3) summaryService.summarySortedBySpentTime(Input.isSummarySortOrderDesc());
             if (menuNum == 4) summaryService.summarySortedByTotalPayment(Input.isSummarySortOrderDesc());
         } catch (InputEndException exception) {
             System.out.println(exception.getMessage());
+        }
+    }
+
+    private void printSummaryDefault() {
+        try {
+            ArrayList<ArrayList<Customer>> allCustomers = summaryService.summaryDefault();
+            printSummary(allCustomers);
+        } catch (ArrayEmptyException exception) {
+            Output.printErrorMessage(exception.getMessage());
+        }
+    }
+
+    // 뭔가 잘못됨이 느껴지는 메서드 나중에 처리할 것
+    private void printSummary(ArrayList<ArrayList<Customer>> allCustomers) {
+        try {
+            List<Group> sortGroup = groupService.sortGroup();
+            Collections.reverse(allCustomers);
+            Collections.reverse(sortGroup);
+            Output.customerClassifiedList(allCustomers, sortGroup);
+        } catch (NullPointerException | IndexOutOfBoundsException exception) {
+            System.out.println("please Complete Groups setting");
         }
     }
 }

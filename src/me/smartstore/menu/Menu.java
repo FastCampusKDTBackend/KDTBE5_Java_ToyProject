69c9e73ddf4eb675f15719082bbd8fccc76a1d77
Ussu1112 <ussu1112@naver.com>
2023-05-07 23:36:10 +0900

    클래스 구현 및 강의 복습 후 소스 작성

--- src/me/smartstore/menu/Menu.java
+++ src/me/smartstore/menu/Menu.java
@@ -1,4 +1,42 @@
 package me.smartstore.menu;
 
-public class Menu {
+import me.smartstore.exception.InputEndException;
+import me.smartstore.exception.InputRangeException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public interface Menu {
+    Scanner scanner = new Scanner(System.in);
+
+    default String nextLine() {
+        return scanner.nextLine().toUpperCase();
+    }
+
+    default String nextLine(String end) {
+        System.out.println("** Press 'end', if you want to exit! **");
+        String str = scanner.nextLine().toUpperCase();
+        if (str.equals(end)) throw new InputEndException();
+        return str;
+    }
+
+    default int chooseMenu(String[] menus) {
+        while (true){
+            try {
+                System.out.println("===============================");
+                for (int i = 0; i < menus.length; i++) {
+                    System.out.printf(" %d. %s\n", i + 1, menus[i]);
+                }
+                System.out.println("===============================");
+                System.out.print("Choose One: ");
+                int choice = Integer.parseInt(nextLine());
+                if (choice >= 1 && choice <= menus.length) return choice;
+                throw new InputRangeException(); // choice 가 범위에 벗어남
+            } catch (InputMismatchException | InputRangeException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    void manage(); // 각 서브메뉴들을 관리하는 함수 (각 서브메뉴의 최상위 메뉴)
 }

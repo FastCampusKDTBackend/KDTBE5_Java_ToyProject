c003db51c03c9b492dd960e846dd14cbef80d893
jy-b <jongyoonb+u@gmail.com>
2023-05-08 18:10:38 +0900

    refactor: Modify smartstore class
    [
    Motified :
     Add Load initial screen
    ]

--- src/com/smartstore/SmartStore.java
+++ src/com/smartstore/SmartStore.java
@@ -1,7 +1,11 @@
 package com.smartstore;
 
+import com.smartstore.menu.mainmenu.MainMenuFunction;
+
 public class SmartStore {
     public static void run(){
-
+        //Load Initial Screen
+        MainMenuFunction mainMenuFunction = MainMenuFunction.of(0);
+        mainMenuFunction.run();
     }
 }

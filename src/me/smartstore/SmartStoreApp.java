83403044e7b82d5cab2566e2832990b3cf8ac58d
seonghye0n <seonghye0n@kakao.com>
2023-05-08 17:11:05 +0900

    프로그램 실행을 위한 Main 클래스 추가
    SmartStoreApp 객체를 추가하여 테스트 환경 및 실제 환경 구동을 메서드로 분리하였습니다.

---
+++ src/me/smartstore/SmartStoreApp.java
@@ -0,0 +1,57 @@
+package me.smartstore;
+
+import me.smartstore.customer.Customer;
+import me.smartstore.customer.Customers;
+import me.smartstore.group.Group;
+import me.smartstore.group.GroupType;
+import me.smartstore.group.Groups;
+import me.smartstore.menu.MainMenu;
+
+public class SmartStoreApp {
+    private final Groups allGroups = Groups.getInstance();
+    private final Customers allCustomers = Customers.getInstance();
+    private static SmartStoreApp smartStoreApp;
+
+    private SmartStoreApp() {}
+
+    public static SmartStoreApp getInstance() {
+        if (smartStoreApp == null) {
+            smartStoreApp = new SmartStoreApp();
+        }
+
+        return smartStoreApp;
+    }
+
+    public void details() {
+        System.out.println("==========================================");
+        System.out.println("Title : SmartStore Project");
+        System.out.println("Release Date : 23.04.28");
+        System.out.println("==========================================");
+    }
+    public SmartStoreApp test() {
+        allGroups.add(new Group(GroupType.G, 10, 100000));
+        allGroups.add(new Group(GroupType.V, 20, 200000));
+        allGroups.add(new Group(GroupType.VV, 30, 300000));
+
+        System.out.println("================== TEST GROUPS ==================");
+        System.out.println(allGroups);
+
+        for (int i = 0; i < 20; i++) {
+            allCustomers.add(new Customer(Character.toString((char) ('a' + i)),
+                    (char) ('a' + i) + "id",
+                    ((int) (Math.random() * 5) + 1) * 10,
+                    ((int) (Math.random() * 5) + 1) * 100000));
+        }
+
+        //allCustomers.refresh();
+
+        System.out.println("================== TEST CUSTOMERS ==================");
+        System.out.println(allCustomers);
+        return this;
+    }
+
+    public void run() {
+        details();
+        MainMenu.getInstance().show();
+    }
+}

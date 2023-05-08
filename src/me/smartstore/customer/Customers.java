8066710888b39f80708b2a81f11db670a1a1043e
seonghye0n <seonghye0n@kakao.com>
2023-05-08 17:34:27 +0900

    분류 기준에 따라 고객 등급을 분류하는 refresh 메서드 추가

--- src/me/smartstore/customer/Customers.java
+++ src/me/smartstore/customer/Customers.java
@@ -1,2 +1,34 @@
-package me.smartstore.customer;public class Customers {
+package me.smartstore.customer;
+
+import me.smartstore.array.Array;
+import me.smartstore.group.Group;
+import me.smartstore.group.Groups;
+
+public class Customers extends Array<Customer> {
+	private static Customers allCustomers;
+	private static Groups allGroups = Groups.getInstance();
+
+	private Customers() {}
+
+	public static Customers getInstance() {
+		if (allCustomers == null) {
+			allCustomers = new Customers();
+		}
+
+		return allCustomers;
+	}
+
+	public void refresh() {
+		for (int i = 0; i < allCustomers.size(); i++) {
+			Customer customer = allCustomers.get(i);
+
+			for (int j = 0; j < allGroups.size(); j++) {
+				Group group = allGroups.get(j);
+
+				if (customer.getUseHours() >= group.getMinHours() && customer.getCustomerPay() >= group.getMinPay()) {
+					customer.setCustomerGroup(group);
+				}
+			}
+		}
+	}
 }

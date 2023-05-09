a1a9e71fe73ff7c4dea9cf32b0759ee449282197
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-05-10 04:27:25 +0900

    refactor: 상수 문자열 수정

--- smartstore/src/main/java/util/view/OutputView.java
+++ smartstore/src/main/java/util/view/OutputView.java
@@ -1,52 +1,51 @@
 package util.view;
 
 import domain.menu.Menu;
-import util.common.ViewMessage;
 
 import java.util.Arrays;
 
 public class OutputView {
-    public static void viewMenus(Menu[] menus) {
+
+    private OutputView() {
+    }
+
+    public static void showMenus(Menu[] menus) {
         System.out.println(generateMenuFormat(menus));
     }
 
-    public static void chooseMenu() {
-        System.out.print(ViewMessage.INPUT_MENU);
+    public static void showMessage(ViewMessage viewMessage) {
+        System.out.println(viewMessage.getMessage());
     }
 
-    public static void viewExitGuide() {
-        System.out.println("** Press '" +ViewMessage.EXIT_CHOICE+"', if you want to exit! **");
-    }
-    public static void chooseGroup(String format) {
-        System.out.println("Which type ("+ format +")?  \n");
-        viewExitGuide();
+    public static void showExitGuide() {
+        System.out.println("** Press '" + ViewMessage.QUIT_MENU_NAME.getMessage() + "', if you want to exit! **");
     }
 
-    public static void viewErrorMessage(String errorMessage) {
+    public static void chooseType(String format) {
+        System.out.println("Which type (" + format + ")?  \n");
+        showExitGuide();
+    }
+
+    public static void showSelectCustomerMessage(int size) {
+        System.out.print("Which customer ( 1 ~ " + size + " )?");
+    }
+
+    public static void showErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
-    }
-
-    public static void chooseParameterForSetting() {
-        System.out.println("==============================\n" +
-                " 1. Minimum Spent Time\n" +
-                " 2. Minimum Total Pay\n" +
-                " 3. Back\n" +
-                "==============================\n");
-
-        chooseMenu();
     }
 
     private static String generateMenuFormat(Menu[] menus) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(ViewMessage.SEPERATOR);
+        stringBuilder.append(ViewMessage.SEPERATOR.getMessage());
 
-        Arrays.stream(menus).forEach(menu -> {
-            stringBuilder.append(menu.toString());
-        });
+        Arrays.stream(menus).forEach(menu -> stringBuilder.append(menu.toString()));
 
-        stringBuilder.append("  " + (menus.length + 1) + ". " + ViewMessage.QUIT + "\n");
-        stringBuilder.append(ViewMessage.SEPERATOR);
+        stringBuilder.append("  " + (menus.length + 1) + ". " + ViewMessage.QUIT_MENU_NAME.getMessage() + "\n");
+        stringBuilder.append(ViewMessage.SEPERATOR.getMessage());
         return stringBuilder.toString();
     }
 
+    public static void showInfo(String info) {
+        System.out.println(info);
+    }
 }

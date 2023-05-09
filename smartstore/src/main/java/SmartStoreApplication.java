8bfb199eac72f379adf9240e309f1e235d4dce5c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-05-09 23:47:48 +0900

    refactor: MainMenu로 로직 분리

--- smartstore/src/main/java/SmartStoreApplication.java
+++ smartstore/src/main/java/SmartStoreApplication.java
@@ -1,13 +1,7 @@
 import domain.menu.main.MainMenu;
-import util.common.ErrorMessage;
-import util.view.InputScanner;
-import util.view.OutputView;
-
-import java.util.Scanner;
 
 public class SmartStoreApplication {
     private static SmartStoreApplication smartStoreApplication = null;
-    private final Scanner scanner = InputScanner.get();
 
     private SmartStoreApplication() {
     }
@@ -16,39 +10,10 @@ public class SmartStoreApplication {
         if (smartStoreApplication == null) {
             smartStoreApplication = new SmartStoreApplication();
         }
-
         return smartStoreApplication;
     }
 
     public void run() {
         MainMenu.executeMain();
-    }
-
-    private int getMenuNumber() {
-        while (true) {
-            try {
-                return Integer.parseInt(scanner.nextLine());
-            } catch (RuntimeException runtimeException) {
-                OutputView.viewErrorMessage(ErrorMessage.INVALID_INPUT);
-            }
-        }
-    }
-
-    private boolean isFinish(int menuNumber) {
-        return MainMenu.isQuit(menuNumber);
-    }
-
-    private void executeMenu(int menuNumber) {
-        try {
-            MainMenu.findByNumber(menuNumber)
-                    .ifPresentOrElse(
-                            MainMenu::execute,
-                            () -> {
-                                throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT);
-                            }
-                    );
-        } catch (IllegalArgumentException e) {
-            OutputView.viewErrorMessage(e.getMessage());
-        }
     }
 }

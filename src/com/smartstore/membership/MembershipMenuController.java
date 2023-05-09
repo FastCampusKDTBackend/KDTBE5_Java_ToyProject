c1ca6b539e02de6195ff2a15346b91dbb161efd2
jy-b <jongyoonb+u@gmail.com>
2023-05-09 19:21:32 +0900

    refactor: Add MembershipMenuController
    [Added : Inter face forMembershipMenu Control
     implement Menu]

---
+++ src/com/smartstore/membership/MembershipMenuController.java
@@ -0,0 +1,65 @@
+package com.smartstore.membership;
+
+import com.smartstore.menu.Menu;
+import com.smartstore.util.CustomList;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+public interface MembershipMenuController extends Menu {
+    @Override
+    default void runMenuSelectionLoop(String[] memberships) {
+        String memberTypeName="";
+        boolean isExit = false;
+        if(memberships.length > 0) {
+            while (!isExit) {
+                try {
+                    System.out.print("Input : ");
+                    memberTypeName = br.readLine();
+                    for (String membershipName : memberships) {
+                        if (MembershipType.valueOf(membershipName).isMatchedName(memberTypeName)) {
+                            isExit = true;
+                            break;
+                        }
+                    }
+                } catch (IOException | NullPointerException e) {
+                    System.out.println("Invalid Menu");
+                }
+            }
+            handleChoice(memberTypeName);
+        }
+    }
+
+    @Override
+    default void displayMenu(String[] menus){
+        System.out.print("Which One?\n|");
+        for(String menu : menus){
+            System.out.printf(" %s |",menu);
+        }
+        System.out.println();
+    }
+
+    default String[] getEnumValues(){
+        CustomList<String> keyList = new CustomList<>();
+        for(MembershipType membershipType : MembershipType.values()){
+            keyList.add(membershipType.name());
+        }
+        return keyList.toArray(String[].class);
+    }
+
+    default MembershipType getMembershipType(String membershipNames){
+        String[] values = getEnumValues();
+        for (String membershipName : values) {
+            try{
+                if (MembershipType.valueOf(membershipName).findByName(membershipNames)) {
+                    return MembershipType.valueOf(membershipName);
+                }
+            }catch (NoSuchElementException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+
+}

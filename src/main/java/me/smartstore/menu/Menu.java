f658e9c86e435e88ea5f3029faad14bf93418761
brotherstone97 <brotherstone97@gmail.com>
2023-05-09 19:20:10 +0900

    feat(menu): 초기 메뉴 기능 추가

--- src/main/java/me/smartstore/menu/Menu.java
+++ src/main/java/me/smartstore/menu/Menu.java
@@ -1,13 +1,45 @@
 package me.smartstore.menu;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.InputMismatchException;
 
 public class Menu {
     protected BufferedReader br;
 
     public Menu() {
         br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    protected void initMenu() {
+        printInitMenu();
+        int menuNumber = 0;
+        while (menuNumber < 1 || menuNumber > 4) {
+            try {
+                menuNumber = readInt();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void printInitMenu() {
+        System.out.println("==============================");
+        System.out.println("1. 고객 등급 기준 설정");
+        System.out.println("2. 고객 정보");
+        System.out.println("3. 등급별 고객 정보 요약");
+        System.out.println("4. 프로그램 종료");
+        System.out.println("==============================");
+        System.out.print("메뉴번호를 입력해주세요: ");
+
+    }
+
+    //@Todo 예외처리
+    private int readInt() throws IOException {
+        String input = br.readLine();
+        int newInt = Integer.parseInt(input);
+        return newInt;
     }
 
     void addData() {

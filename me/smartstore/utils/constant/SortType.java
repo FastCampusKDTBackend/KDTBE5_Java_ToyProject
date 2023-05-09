551a614ba77d5862068d2d35133b02ec507a8a15
khsrla9806 <kimhunsope@naver.com>
2023-05-09 12:32:47 +0900

    chore: 뛰어쓰기, 개행 수정

--- me/smartstore/utils/constant/SortType.java
+++ me/smartstore/utils/constant/SortType.java
@@ -10,9 +10,9 @@ public enum SortType {
 
     /**
      * 정렬 기준을 입력으로 받아, 해당 타입에 해당하는 Comparator를 정렬 기준에 맞게 반환합니다.
-     * @param classificationVal: -1인 경우 내림차순, 1인 경우 오름차순을 의미합니다.
+     * @param classificationVal -1인 경우 내림차순, 1인 경우 오름차순을 의미합니다.
      * @return 입력받은 정렬 기준을 사용하여 알맞는 Comparator를 반환
-     * @throws InvalidSortTypeException: SortType에 존재하지 않는 값이 들어오면 예외를 발생
+     * @throws InvalidSortTypeException SortType에 존재하지 않는 값이 들어오면 예외를 발생
      */
     public Comparator<Customer> getCustomerComparator(int classificationVal) {
         if (this == CUSTOMER_NAME) {
@@ -25,6 +25,7 @@ public enum SortType {
             return (customer1, customer2) ->
                     classificationVal * customer1.getTotalPay().compareTo(customer2.getTotalPay());
         }
+
         throw new InvalidSortTypeException();
     }
 }

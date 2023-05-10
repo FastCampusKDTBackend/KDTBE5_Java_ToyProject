1c63099ed5ea4e1e4e396e3ae19438a90017ea10
YongHo Shin <gdtknight@naver.com>
2023-05-11 04:59:23 +0900

    fix: update ClassificationSummaryMenu
    - 싱글톤 생성 관련 오류 수정
    - 주석 추가

--- src/main/java/me/smartstore/core/view/ClassificationSummaryMenu.java
+++ src/main/java/me/smartstore/core/view/ClassificationSummaryMenu.java
@@ -11,8 +11,15 @@ import me.smartstore.enums.SortOrder;
 import me.smartstore.exceptions.StoreException;
 import me.smartstore.utils.ScannerUtility;
 
+/**
+ * 고객 분류 정보 출력 메뉴
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class ClassificationSummaryMenu extends AbstractMenu {
-  private static final ClassificationSummaryMenu classificationSummaryMenu =
+  private static ClassificationSummaryMenu classificationSummaryMenu =
       new ClassificationSummaryMenu();
 
   private static final CustomerService customerService = CustomerService.getInstance();
@@ -28,16 +35,59 @@ public class ClassificationSummaryMenu extends AbstractMenu {
         });
   }
 
-  public static void launch() {
+  public static ClassificationSummaryMenu getInstance() {
+    if (classificationSummaryMenu == null) {
+      classificationSummaryMenu = new ClassificationSummaryMenu();
+    }
+    return classificationSummaryMenu;
+  }
+
+  public void launch() {
     loop:
     while (true) {
       classificationSummaryMenu.show();
       try {
         switch (classificationSummaryMenu.selectMenuNumber()) {
+            // 일반 요약 정보 출력 - 가장 최근에 조회한 정렬 기준에 따라 자동으로 변환
           case 1 -> customerService.displayClassificationSummary();
-          case 2 -> customerService.displayClassificationSummary(NAME, inputSortOrder());
-          case 3 -> customerService.displayClassificationSummary(SPENT_TIME, inputSortOrder());
-          case 4 -> customerService.displayClassificationSummary(PAY_AMOUNT, inputSortOrder());
+
+            // 고객 그룹별 이름순 정렬
+          case 2 -> {
+            while (true) {
+              try {
+                customerService.displayClassificationSummary(NAME, inputSortOrder());
+              } catch (StoreException e) {
+                if (e.getErrorCode() == INPUT_END) break;
+                System.out.println(e.getMessage());
+              }
+            }
+          }
+
+            // 고객 그룹별 이용시간순 정렬
+          case 3 -> {
+            while (true) {
+              try {
+                customerService.displayClassificationSummary(SPENT_TIME, inputSortOrder());
+              } catch (StoreException e) {
+                if (e.getErrorCode() == INPUT_END) break;
+                System.out.println(e.getMessage());
+              }
+            }
+          }
+
+            // 고객 그룹별 사용금액순 정렬
+          case 4 -> {
+            while (true) {
+              try {
+                customerService.displayClassificationSummary(PAY_AMOUNT, inputSortOrder());
+              } catch (StoreException e) {
+                if (e.getErrorCode() == INPUT_END) break;
+                System.out.println(e.getMessage());
+              }
+            }
+          }
+
+            // 이전 메뉴
           case 5 -> {
             break loop;
           }
@@ -48,7 +98,11 @@ public class ClassificationSummaryMenu extends AbstractMenu {
     }
   }
 
-  private static SortOrder inputSortOrder() {
+  /**
+   * @return 정렬 방향 (오름차순, 내림차순)
+   * @throws StoreException 종료 선택시
+   */
+  private static SortOrder inputSortOrder() throws StoreException {
     while (true) {
       System.out.println(INPUT_SORT_ORDER + "\n" + PRESS_END_MSG);
       try {
@@ -57,6 +111,7 @@ public class ClassificationSummaryMenu extends AbstractMenu {
         return convertInputStrToSortOrder(input);
       } catch (StoreException e) {
         System.out.println(e.getMessage());
+        if (e.getErrorCode() == INPUT_END) throw new StoreException(INPUT_END);
       }
     }
   }

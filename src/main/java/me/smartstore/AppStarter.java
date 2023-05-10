2d2b6f84474277aeef29a75873ccc7b79104bfbb
YongHo Shin <gdtknight@naver.com>
2023-05-11 00:07:06 +0900

    comment: add javadoc - SmartStoreApplication, AppStarter

--- src/main/java/me/smartstore/AppStarter.java
+++ src/main/java/me/smartstore/AppStarter.java
@@ -2,6 +2,13 @@ package me.smartstore;
 
 import me.smartstore.core.view.MainMenu;
 
+/**
+ * 스마트스토어 메인 시작점
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class AppStarter {
   private AppStarter() {}
 
--- src/main/java/me/smartstore/SmartStoreApplication.java
+++ src/main/java/me/smartstore/SmartStoreApplication.java
@@ -1,5 +1,12 @@
 package me.smartstore;
 
+/**
+ * 스마트스토어 애플리케이션
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class SmartStoreApplication {
   public static void main(String[] args) {
     AppStarter.getInstance().run();

a8fe309126a711387ba3d3298fce7f1109aa659c
brotherstone97 <brotherstone97@gmail.com>
2023-05-10 22:52:03 +0900

    refactor(GroupMenu): addData() 메서드 분리

--- src/main/java/me/smartstore/menu/GroupMenu.java
+++ src/main/java/me/smartstore/menu/GroupMenu.java
@@ -10,10 +10,28 @@ public class GroupMenu extends Menu implements DataCRUD {
     @Override
     public void addData() {
         //변경할 등급 선택
+        printGroupSelectionMessage();
+        int groupNumber = inputGroupNumber();
+        //groupNumber to group
+        GroupType groupType = groupNumberToGroupType(groupNumber);
+        //groupList에서 입력받은 등급에 해당하는 요소를 찾음
+        if (insertParameter(groupType)) {
+            System.out.println("기준이 성공적으로 입력되었습니다.");
+        }
+
+    }
+
+
+    private void printGroupSelectionMessage() {
         System.out.println("다음 등급 중 하나를 선택해주세요.");
         System.out.println("1. General");
         System.out.println("2. VIP");
         System.out.println("3. VVIP");
+        System.out.println("==============================");
+        System.out.print("번호를 입력해주세요: ");
+    }
+
+    private int inputGroupNumber() {
         int groupNumber = 0;
         while (groupNumber == 0) {
             try {
@@ -27,28 +45,7 @@ public class GroupMenu extends Menu implements DataCRUD {
                 System.out.println(e.getMessage());
             }
         }
-
-        //groupNumber to group
-        GroupType groupType = groupNumberToGroupType(groupNumber);
-        MyArrayList<Group> groupList = groups.getGroups();
-        for (Group group : groupList) {
-            if (group.getCustomerGroup() == groupType) {
-                if (group.getParameter().getMinimumHours() != 0 || group.getParameter().getMinimumTotalAmount() != 0) {
-                    System.out.println("이미 기준이 등록된 등급입니다.");
-                    return;
-                }//입력할 메뉴 입력 1: 누적 시간/ 2: 누적 결제 금액
-                int parameterNumber = Integer.parseInt(br.readLine());
-                //누적 시간 입력 받음
-                if (parameterNumber == 1) {
-                    int hours = Integer.parseInt(br.readLine());
-                    group.getParameter().setMinimumHours(hours);
-                    return;
-                }
-                int totalAmount = Integer.parseInt(br.readLine());
-                group.getParameter().setMinimumTotalAmount(totalAmount);
-                return;
-            }
-        }
+        return groupNumber;
     }
 
     private GroupType groupNumberToGroupType(int groupNumber) {
@@ -59,6 +56,33 @@ public class GroupMenu extends Menu implements DataCRUD {
             return GroupType.VIP;
         }
         return GroupType.VVIP;
+    }
+
+    private boolean insertParameter(GroupType groupType) {
+        MyArrayList<Group> groupList = groups.getGroups();
+        for (Group group : groupList) {
+            if (group.getCustomerGroup() == groupType) {
+                //찾은 요소가 이미 입력이 된 경우 return
+                if (group.getParameter().getMinimumHours() != 0 || group.getParameter().getMinimumTotalAmount() != 0) {
+                    System.out.println("이미 기준이 등록된 등급입니다.");
+                    return false;
+                }//기준을 입력할 메뉴 입력 1: 누적 시간/ 2: 누적 결제 금액
+                try {
+                    int parameterNumber = Integer.parseInt(br.readLine());
+                    //누적 시간 입력 받음
+                    if (parameterNumber == 1) {
+                        int hours = Integer.parseInt(br.readLine());
+                        group.getParameter().setMinimumHours(hours);
+                        return true;
+                    }
+                    int totalAmount = Integer.parseInt(br.readLine());
+                    group.getParameter().setMinimumTotalAmount(totalAmount);
+                    return true;
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 
     @Override

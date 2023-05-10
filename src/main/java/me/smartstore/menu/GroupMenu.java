1fd4e369765b6bca500d284f5bc27dce3f943ea8
brotherstone97 <brotherstone97@gmail.com>
2023-05-10 22:36:54 +0900

    feat(GroupMenu): 고객 등급 기준 추가 기능 구현

--- src/main/java/me/smartstore/menu/GroupMenu.java
+++ src/main/java/me/smartstore/menu/GroupMenu.java
@@ -1,9 +1,64 @@
 package me.smartstore.menu;
 
-public class GroupMenu extends Menu implements DataCRUD{
+import me.smartstore.collections.MyArrayList;
+import me.smartstore.group.Group;
+import me.smartstore.group.GroupType;
+
+import java.io.IOException;
+
+public class GroupMenu extends Menu implements DataCRUD {
     @Override
     public void addData() {
+        //변경할 등급 선택
+        System.out.println("다음 등급 중 하나를 선택해주세요.");
+        System.out.println("1. General");
+        System.out.println("2. VIP");
+        System.out.println("3. VVIP");
+        int groupNumber = 0;
+        while (groupNumber == 0) {
+            try {
+                int temp = Integer.parseInt(br.readLine());
+                if (temp >= 1 && temp <= 3) {
+                    groupNumber = temp;
+                } else {
+                    System.out.println("1부터 3까지의 숫자를 입력해주세요.");
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
+        //groupNumber to group
+        GroupType groupType = groupNumberToGroupType(groupNumber);
+        MyArrayList<Group> groupList = groups.getGroups();
+        for (Group group : groupList) {
+            if (group.getCustomerGroup() == groupType) {
+                if (group.getParameter().getMinimumHours() != 0 || group.getParameter().getMinimumTotalAmount() != 0) {
+                    System.out.println("이미 기준이 등록된 등급입니다.");
+                    return;
+                }//입력할 메뉴 입력 1: 누적 시간/ 2: 누적 결제 금액
+                int parameterNumber = Integer.parseInt(br.readLine());
+                //누적 시간 입력 받음
+                if (parameterNumber == 1) {
+                    int hours = Integer.parseInt(br.readLine());
+                    group.getParameter().setMinimumHours(hours);
+                    return;
+                }
+                int totalAmount = Integer.parseInt(br.readLine());
+                group.getParameter().setMinimumTotalAmount(totalAmount);
+                return;
+            }
+        }
+    }
+
+    private GroupType groupNumberToGroupType(int groupNumber) {
+        if (groupNumber == 1) {
+            return GroupType.General;
+        }
+        if (groupNumber == 2) {
+            return GroupType.VIP;
+        }
+        return GroupType.VVIP;
     }
 
     @Override

c0a1f7fd9407232f21098ac9bdac0dc1c64f723f
WooSeok77 <wi8tkf@naver.com>
2023-05-10 21:59:26 +0900

    chore : 3차 일부수정

--- me/smartstore/customer/Customers.java
+++ me/smartstore/customer/Customers.java
@@ -88,7 +88,7 @@ public class Customers extends DArray<Customer> {
         Arrays.sort(value, new Comparator<Customer>() {
             @Override
             public int compare(Customer o1, Customer o2) {
-                return (o1.getCusTotalTime().compareTo(o2.getCusTotalPay()) * scount);
+                return (o1.getCusTotalTime().compareTo(o2.getCusTotalTime()) * scount);
             }
         });
     }

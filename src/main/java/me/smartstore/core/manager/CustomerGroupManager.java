6572b1e27db912fc29269e3f3e4925a8168206a4
YongHo Shin <gdtknight@naver.com>
2023-05-11 02:57:48 +0900

    feat: refactor CustomerGroupManager
    - 고객 그룹 정보를 저장하는 데이터베이스라는 취지에 맞게 불필요한 비즈니스 로직을 제거하고 저장, 조회 기능 구현에 충실하도록 리팩토링

--- src/main/java/me/smartstore/core/manager/CustomerGroupManager.java
+++ src/main/java/me/smartstore/core/manager/CustomerGroupManager.java
@@ -5,12 +5,11 @@ import static me.smartstore.exceptions.StoreErrorCode.*;
 
 import java.util.Arrays;
 import me.smartstore.core.domain.CustomerGroup;
-import me.smartstore.core.domain.Parameter;
 import me.smartstore.enums.CustomerType;
 import me.smartstore.exceptions.StoreException;
 
 /**
- * 고객 그룹, 세분화 기준 관리
+ * 스마트스토어의 모든 고객 그룹, 세분화 기준 저장 (Database 대체용)
  *
  * @author YongHo Shin
  * @version v1.0
@@ -35,11 +34,27 @@ public class CustomerGroupManager {
   }
 
   /**
+   * 설정이 완료된 고객 그룹 저장
+   *
+   * @param customerGroup 고객 그룹
+   * @return 저장된 고객 그룹
+   */
+  public CustomerGroup save(CustomerGroup customerGroup) {
+    for (int idx = 0; idx < customerGroups.length; idx++) {
+      if (customerGroup.getCustomerType() == customerGroups[idx].getCustomerType()) {
+        customerGroups[idx] = customerGroup;
+      }
+    }
+    return customerGroup;
+  }
+
+  /**
    * @param customerType 고객 유형
    * @return 일치하는 그룹
    * @throws StoreException 일치하는 그룹 없음
    */
-  public CustomerGroup select(CustomerType customerType) throws StoreException {
+  public CustomerGroup selectCustomerGroupByCustomerType(CustomerType customerType)
+      throws StoreException {
     return Arrays.stream(customerGroups)
         .filter(customerGroup -> customerGroup.getCustomerType() == customerType)
         .findFirst()
@@ -52,33 +67,7 @@ public class CustomerGroupManager {
   /**
    * @return 모든 고객 그룹
    */
-  public CustomerGroup[] selectAll() {
+  public CustomerGroup[] selectAllCustomerGroup() {
     return Arrays.stream(customerGroups).toArray(CustomerGroup[]::new);
-  }
-
-  /**
-   * 그룹별 세분화 기준 저장
-   *
-   * @param customerType 고객 유형
-   * @param parameter 세부화 기준
-   * @return 설정이 완료된 고객 그룹
-   */
-  public CustomerGroup save(CustomerType customerType, Parameter parameter) {
-    CustomerGroup customerGroup = select(customerType);
-
-    if (customerGroup.getParameter() == null) {
-      customerGroup.setParameter(parameter);
-      return customerGroup;
-    }
-
-    if (parameter.getMinSpentTime() != null) {
-      customerGroup.getParameter().setMinSpentTime(parameter.getMinSpentTime());
-    }
-
-    if (parameter.getMinPayAmount() != null) {
-      customerGroup.getParameter().setMinPayAmount(parameter.getMinPayAmount());
-    }
-
-    return customerGroup;
   }
 }

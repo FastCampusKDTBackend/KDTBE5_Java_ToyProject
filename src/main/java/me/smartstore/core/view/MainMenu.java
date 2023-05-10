4a6317ded348f735be8d298cae2d279ace32cdeb
YongHo Shin <gdtknight@naver.com>
2023-05-11 00:19:55 +0900

    comment: add javadoc & comments - MainMenu

--- src/main/java/me/smartstore/core/view/MainMenu.java
+++ src/main/java/me/smartstore/core/view/MainMenu.java
@@ -2,6 +2,13 @@ package me.smartstore.core.view;
 
 import me.smartstore.exceptions.StoreException;
 
+/**
+ * 메인 메뉴 화면
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class MainMenu extends AbstractMenu {
   private static final MainMenu mainMenu = new MainMenu();
 
@@ -15,9 +22,16 @@ public class MainMenu extends AbstractMenu {
       mainMenu.show();
       try {
         switch (mainMenu.selectMenuNumber()) {
+          // 고객 유형 세분화 기준 설정
           case 1 -> ParameterMenu.launch();
+
+          // 고객 정보 관리
           case 2 -> CustomerMenu.launch();
+
+          // 고객 정보 요약
           case 3 -> ClassificationSummaryMenu.launch();
+
+          // 종료
           case 4 -> {
             break loop;
           }

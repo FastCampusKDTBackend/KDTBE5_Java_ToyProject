5a623a5ce5930bdfe89b695356a999df37a694a0
brotherstone97 <brotherstone97@gmail.com>
2023-05-10 23:13:17 +0900

    refactor(GroupMenu): 겹치는 메서드 추가 분리 및 updateData() 구현

--- src/main/java/me/smartstore/menu/GroupMenu.java
+++ src/main/java/me/smartstore/menu/GroupMenu.java
@@ -3,8 +3,10 @@ package me.smartstore.menu;
 import me.smartstore.collections.MyArrayList;
 import me.smartstore.group.Group;
 import me.smartstore.group.GroupType;
+import me.smartstore.group.Parameter;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 public class GroupMenu extends Menu implements DataCRUD {
     @Override
@@ -15,9 +17,7 @@ public class GroupMenu extends Menu implements DataCRUD {
         //groupNumber to group
         GroupType groupType = groupNumberToGroupType(groupNumber);
         //groupList에서 입력받은 등급에 해당하는 요소를 찾음
-        if (insertParameter(groupType)) {
-            System.out.println("기준이 성공적으로 입력되었습니다.");
-        }
+        insert(groupType);
 
     }
 
@@ -58,32 +58,42 @@ public class GroupMenu extends Menu implements DataCRUD {
         return GroupType.VVIP;
     }
 
-    private boolean insertParameter(GroupType groupType) {
+    private void insert(GroupType groupType) {
+        Parameter parameter = findParameter(groupType);
+        //찾은 요소가 이미 입력이 된 경우 return
+        if (parameter.getMinimumHours() != 0 || parameter.getMinimumTotalAmount() != 0) {
+            System.out.println("이미 기준이 등록된 등급입니다.");
+            return;
+        }
+        insertInputParameter(parameter);
+    }
+
+    private void insertInputParameter(Parameter parameter) {
+        //기준을 입력할 메뉴 입력 1: 누적 시간/ 2: 누적 결제 금액
+        try {
+            int parameterNumber = Integer.parseInt(br.readLine());
+            //누적 시간 입력 받음
+            if (parameterNumber == 1) {
+                int hours = Integer.parseInt(br.readLine());
+                parameter.setMinimumHours(hours);
+                return;
+            }
+            //누적 결제 금액 입력 받음
+            int totalAmount = Integer.parseInt(br.readLine());
+            parameter.setMinimumTotalAmount(totalAmount);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Parameter findParameter(GroupType groupType) {
         MyArrayList<Group> groupList = groups.getGroups();
         for (Group group : groupList) {
             if (group.getCustomerGroup() == groupType) {
-                //찾은 요소가 이미 입력이 된 경우 return
-                if (group.getParameter().getMinimumHours() != 0 || group.getParameter().getMinimumTotalAmount() != 0) {
-                    System.out.println("이미 기준이 등록된 등급입니다.");
-                    return false;
-                }//기준을 입력할 메뉴 입력 1: 누적 시간/ 2: 누적 결제 금액
-                try {
-                    int parameterNumber = Integer.parseInt(br.readLine());
-                    //누적 시간 입력 받음
-                    if (parameterNumber == 1) {
-                        int hours = Integer.parseInt(br.readLine());
-                        group.getParameter().setMinimumHours(hours);
-                        return true;
-                    }
-                    int totalAmount = Integer.parseInt(br.readLine());
-                    group.getParameter().setMinimumTotalAmount(totalAmount);
-                    return true;
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
+                return group.getParameter();
             }
         }
-        return false;
+        throw new NoSuchElementException("해당하는 등급이 존재하지 않습니다.");
     }
 
     @Override
@@ -92,17 +102,17 @@ public class GroupMenu extends Menu implements DataCRUD {
         int groupNumber = inputGroupNumber();
         //groupNumber to group
         GroupType groupType = groupNumberToGroupType(groupNumber);
-        MyArrayList<Group> groupList = groups.getGroups();
-        for (Group group : groupList) {
-            if (group.getCustomerGroup() == groupType) {
-                System.out.println(group.getParameter());
-            }
-        }
+        Parameter parameter = findParameter(groupType);
+        System.out.println(parameter);
     }
 
     @Override
     public void updateData() {
-
+        printGroupSelectionMessage();
+        int groupNumber = inputGroupNumber();
+        GroupType groupType = groupNumberToGroupType(groupNumber);
+        Parameter parameter = findParameter(groupType);
+        insertInputParameter(parameter);
     }
 
     @Override

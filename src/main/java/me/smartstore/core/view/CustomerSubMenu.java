8f2a199f8a3aaea03d47fdfc9ed117358dca2c9f
YongHo Shin <gdtknight@naver.com>
2023-05-11 05:05:29 +0900

    comment: update CustomerSubMenu
    - 주석 추가

--- src/main/java/me/smartstore/core/view/CustomerSubMenu.java
+++ src/main/java/me/smartstore/core/view/CustomerSubMenu.java
@@ -8,6 +8,13 @@ import me.smartstore.exceptions.StoreErrorCode;
 import me.smartstore.exceptions.StoreException;
 import me.smartstore.utils.ScannerUtility;
 
+/**
+ * 고객 세부 정보 관리 메뉴
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class CustomerSubMenu extends AbstractMenu {
   private static final CustomerSubMenu customerSubMenu = new CustomerSubMenu();
 
@@ -22,6 +29,9 @@ public class CustomerSubMenu extends AbstractMenu {
     return customerSubMenu;
   }
 
+  /**
+   * @return 입력 받은 고객 세부 정보
+   */
   public CustomerDTO inputCustomerInfo() {
     String customerName = null;
     String customerID = null;
@@ -51,6 +61,10 @@ public class CustomerSubMenu extends AbstractMenu {
     return CustomerDTO.of(customerName, customerID, customerSpentTime, customerPayAmount);
   }
 
+  /**
+   * @return 입력 받은 고객 이름
+   * @throws StoreException 종료 선택시
+   */
   private String inputCustomerName() throws StoreException {
     System.out.println(INPUT_CUSTOMER_NAME + "\n" + PRESS_END_MSG);
     String input = ScannerUtility.getInput();
@@ -58,6 +72,10 @@ public class CustomerSubMenu extends AbstractMenu {
     return input;
   }
 
+  /**
+   * @return 입력 받은 고객 ID
+   * @throws StoreException 종료 선택시
+   */
   private String inputCustomerId() throws StoreException {
     System.out.println(INPUT_CUSTOMER_ID + "\n" + PRESS_END_MSG);
     String input = ScannerUtility.getInput();
@@ -65,6 +83,10 @@ public class CustomerSubMenu extends AbstractMenu {
     return input;
   }
 
+  /**
+   * @return 입력 받은 고객의 이용 시간
+   * @throws StoreException 종료 선택시
+   */
   private int inputCustomerSpentTime() throws StoreException {
     while (true) {
       System.out.println(INPUT_CUSTOMER_SPENT_TIME + "\n" + PRESS_END_MSG);
@@ -78,6 +100,10 @@ public class CustomerSubMenu extends AbstractMenu {
     }
   }
 
+  /**
+   * @return 입력 받은 고객의 결제 금액
+   * @throws StoreException 종료 선택시
+   */
   private int inputCustomerPayAmount() throws StoreException {
     while (true) {
       System.out.println(INPUT_CUSTOMER_PAY_AMOUNT + "\n" + PRESS_END_MSG);

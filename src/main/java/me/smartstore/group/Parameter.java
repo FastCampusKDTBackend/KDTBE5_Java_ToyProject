aea57110868b45fec70e4cee93fcf042223f1383
brotherstone97 <brotherstone97@gmail.com>
2023-05-10 13:35:16 +0900

    feat: 고객 등급 기준이 되는 Parameter클래스 생성
    Parameter클래스는 최소 이용 시간, 최소 누적 결제 금액을 필드로 갖는다.

---
+++ src/main/java/me/smartstore/group/Parameter.java
@@ -0,0 +1,11 @@
+package me.smartstore.group;
+
+public class Parameter {
+    private int minimumHours;
+    private int minimumTotalAmount;
+
+    public Parameter(int minimumHours, int minimumTotalAmount) {
+        this.minimumHours = minimumHours;
+        this.minimumTotalAmount = minimumTotalAmount;
+    }
+}

481a5278f3a696ea800ee8f9a55ca5051e708f84
YongHo Shin <gdtknight@naver.com>
2023-05-11 05:06:02 +0900

    fix: update MainMenu
    - 싱글톤 생성 관련 오류 수정
    - 주석 추가

--- src/main/java/me/smartstore/core/view/MainMenu.java
+++ src/main/java/me/smartstore/core/view/MainMenu.java
@@ -10,28 +10,35 @@ import me.smartstore.exceptions.StoreException;
  * @since 2023-05-10
  */
 public class MainMenu extends AbstractMenu {
-  private static final MainMenu mainMenu = new MainMenu();
+  private static MainMenu mainMenu = new MainMenu();
 
   private MainMenu() {
     super(new String[] {"Parameter", "Customer Data", "Classification Summary", "Quit"});
   }
 
-  public static void launch() {
+  public static MainMenu getInstance() {
+    if (mainMenu == null) {
+      mainMenu = new MainMenu();
+    }
+    return mainMenu;
+  }
+
+  public void launch() {
     loop:
     while (true) {
       mainMenu.show();
       try {
         switch (mainMenu.selectMenuNumber()) {
-          // 고객 유형 세분화 기준 설정
-          case 1 -> ParameterMenu.launch();
+            // 고객 유형 세분화 기준 설정
+          case 1 -> ParameterMenu.getInstance().launch();
 
-          // 고객 정보 관리
-          case 2 -> CustomerMenu.launch();
+            // 고객 정보 관리
+          case 2 -> CustomerMenu.getInstance().launch();
 
-          // 고객 정보 요약
-          case 3 -> ClassificationSummaryMenu.launch();
+            // 고객 정보 요약
+          case 3 -> ClassificationSummaryMenu.getInstance().launch();
 
-          // 종료
+            // 종료
           case 4 -> {
             break loop;
           }

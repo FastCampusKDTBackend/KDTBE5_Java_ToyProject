008bd25b3eab36021bdef98b1b86be5d7400d371
YongHo Shin <gdtknight@naver.com>
2023-05-11 03:05:31 +0900

    feat: refactor CustomerGroupService
    - 고객 그룹 관리 서비스라는 취지에 맞게 비즈니스 로직을 구현
    - 분류 기준이 업데이트 된 경우 고객 정보도 함께 업데이트
    - 주석 추가

--- src/main/java/me/smartstore/core/service/CustomerGroupService.java
+++ src/main/java/me/smartstore/core/service/CustomerGroupService.java
@@ -6,24 +6,66 @@ import me.smartstore.core.manager.CustomerGroupManager;
 import me.smartstore.enums.CustomerType;
 import me.smartstore.exceptions.StoreException;
 
+/**
+ * 고객 유형 관리 서비스 제공 클래스
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
 public class CustomerGroupService {
   private static final CustomerGroupService customerGroupService = new CustomerGroupService();
   private final CustomerGroupManager customerGroupManager = CustomerGroupManager.getInstance();
+  private final CustomerService customerService = CustomerService.getInstance();
 
   public static CustomerGroupService getInstance() {
     return customerGroupService;
   }
 
+  /**
+   * 고객 그룹 분류 기준 설정. 분류 기준이 새로 설정된 경우 고객 정보도 함께 업데이트
+   *
+   * @param customerType 고객 유형
+   * @param parameter 분류 기준
+   * @return 설정이 완료된 고객 그룹
+   * @throws StoreException 일치하는 고객 그룹이 데이터베이스 없는 경우
+   */
   public CustomerGroup setParameter(CustomerType customerType, Parameter parameter)
       throws StoreException {
-    return customerGroupManager.save(customerType, parameter);
+    CustomerGroup customerGroup =
+        customerGroupManager.selectCustomerGroupByCustomerType(customerType);
+
+    if (customerGroup.getParameter() == null) {
+      customerGroup.setParameter(parameter);
+    } else {
+      if (parameter.getMinSpentTime() != null) {
+        customerGroup.getParameter().setMinSpentTime(parameter.getMinSpentTime());
+      }
+
+      if (parameter.getMinPayAmount() != null) {
+        customerGroup.getParameter().setMinPayAmount(parameter.getMinPayAmount());
+      }
+    }
+
+    customerService.classifyAllCustomers();
+
+    return customerGroupManager.save(customerGroup);
   }
 
+  /**
+   * @param customerType 고객 유형
+   * @return 고객 유형과 일치하는 그룹
+   * @throws StoreException 데이터베이스에 일치하는 그룹이 없을 경우
+   */
   public CustomerGroup find(CustomerType customerType) throws StoreException {
-    return customerGroupManager.select(customerType);
+    return customerGroupManager.selectCustomerGroupByCustomerType(customerType);
   }
 
+  /**
+   * @return 데이터베이스에 저장된 모든 고객 그룹
+   * @throws StoreException 데이터베이스 오류
+   */
   public CustomerGroup[] findAll() throws StoreException {
-    return customerGroupManager.selectAll();
+    return customerGroupManager.selectAllCustomerGroup();
   }
 }

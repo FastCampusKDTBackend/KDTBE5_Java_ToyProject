ab00b7288e2bc490141ce42a260171795f2b73f5
brotherstone97 <brotherstone97@gmail.com>
2023-05-10 17:12:29 +0900

    feat(customer): 고객 정보를 등급별로 분류하기 위한 ClassifiedCustomers클래스 생성

---
+++ src/main/java/me/smartstore/group/ClassifiedCustomers.java
@@ -0,0 +1,47 @@
+package me.smartstore.group;
+
+import me.smartstore.collections.MyArrayList;
+import me.smartstore.customer.Customer;
+import me.smartstore.customer.Customers;
+
+public class ClassifiedCustomers {
+    private MyArrayList<Customer> classifiedByGeneral;
+    private MyArrayList<Customer> classifiedByVIP;
+    private MyArrayList<Customer> classifiedByVVIP;
+
+    private static ClassifiedCustomers classifiedCustomers;
+
+    private ClassifiedCustomers() {
+    }
+
+    public static ClassifiedCustomers getInstance() {
+        if (classifiedCustomers == null) {
+            classifiedCustomers = new ClassifiedCustomers();
+        }
+        return classifiedCustomers;
+    }
+
+    public void refresh() {
+        Customers customers = Customers.getInstance();
+        MyArrayList<Customer> customerList = customers.getCustomers();
+        initCustomerList();
+
+        for (Customer customer : customerList) {
+            if (customer.getGroup() == GroupType.General) {
+                classifiedByGeneral.add(customer);
+                continue;
+            }
+            if (customer.getGroup() == GroupType.VIP) {
+                classifiedByVIP.add(customer);
+                continue;
+            }
+            classifiedByVVIP.add(customer);
+        }
+    }
+
+    private void initCustomerList() {
+        classifiedByGeneral = new MyArrayList<>();
+        classifiedByVIP = new MyArrayList<>();
+        classifiedByVVIP = new MyArrayList<>();
+    }
+}

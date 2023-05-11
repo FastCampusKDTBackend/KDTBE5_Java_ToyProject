8684b848c2bfb68a32c8ef1a909cae4de61fe52b
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-05-11 19:54:51 +0900

    refactor: 가독성을 위한 네이밍 수정

--- smartstore/src/main/java/domain/menu/main/classification/ClassificationMenu.java
+++ smartstore/src/main/java/domain/menu/main/classification/ClassificationMenu.java
@@ -1,12 +1,12 @@
 package domain.menu.main.classification;
 
 import domain.menu.Menu;
-import util.comparator.customer.CompareWithConsumer;
+import util.comparator.customer.CompareWithCustomer;
 
 import java.util.Arrays;
 import java.util.Optional;
 
-import static util.comparator.customer.CompareWithConsumer.*;
+import static util.comparator.customer.CompareWithCustomer.*;
 
 public enum ClassificationMenu implements Menu {
     SUMMARY(
@@ -20,21 +20,21 @@ public enum ClassificationMenu implements Menu {
             2,
             "Summary (Sorted By Name)",
             ClassificationMenuExecute.getMethod(
-                    CompareWithConsumer.by(NAME)
+                    CompareWithCustomer.by(NAME)
             )
     ),
     SORTED_BY_SPENT_TIME(
             3,
             "Summary (Sorted By Spent Time)",
             ClassificationMenuExecute.getMethod(
-                    CompareWithConsumer.by(SPENT_TIME)
+                    CompareWithCustomer.by(SPENT_TIME)
             )
     ),
     SORTED_BY_TOTAL_PAYMENT(
             4,
             "Summary (Sorted By Total Payment)",
             ClassificationMenuExecute.getMethod(
-                    CompareWithConsumer.by(TOTAL_PAYMENT)
+                    CompareWithCustomer.by(TOTAL_PAYMENT)
             )
     );
 
--- smartstore/src/main/java/util/comparator/customer/CompareWithCustomer.java
+++ smartstore/src/main/java/util/comparator/customer/CompareWithCustomer.java
@@ -6,20 +6,20 @@ import util.common.exception.NotFoundException;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public enum CompareWithConsumer {
+public enum CompareWithCustomer {
     NAME(new ComparatorByName<>()),
     SPENT_TIME(new ComparatorBySpentTime<>()),
     TOTAL_PAYMENT(new ComparatorByTotalPayment<>());
 
     private final Comparator<Customer> comparator;
 
-    CompareWithConsumer(Comparator<Customer> comparator) {
+    CompareWithCustomer(Comparator<Customer> comparator) {
         this.comparator = comparator;
     }
 
-    public static Comparator<Customer> by(CompareWithConsumer compareWithConsumer) {
-        return Arrays.stream(CompareWithConsumer.values())
-                .filter(type -> type == compareWithConsumer)
+    public static Comparator<Customer> by(CompareWithCustomer compareWithCustomer) {
+        return Arrays.stream(CompareWithCustomer.values())
+                .filter(type -> type == compareWithCustomer)
                 .map(type -> type.comparator)
                 .findFirst()
                 .orElseThrow(NotFoundException::new);

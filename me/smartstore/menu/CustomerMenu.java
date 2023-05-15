16668d683f8aa81b3daa296ee4cd8bab567560de
seoyonghyun <syh05247@naver.com>
2023-05-15 15:49:30 +0900

    refactor: addCustomer(), updateCustomer() 중복된 코드 리펙터링
    - 코드의 중복을 피하기 위해서 customerInfoInput() 메서드를 이용하여 customer 를 수정하거나 추가할 때 사용하도록 수정

--- me/smartstore/menu/CustomerMenu.java
+++ me/smartstore/menu/CustomerMenu.java
@@ -47,7 +47,15 @@ public class CustomerMenu implements Menu {
                 System.out.println("How many customers to input : ");
                 int inputCount = Integer.parseInt(nextLine(Message.END_MSG));
                 for(int i = 0; i < inputCount; i++) {
-                    customerInfoInput(i + 1);
+                    System.out.printf("=============== Customer %d Info. ===============\n", i + 1);
+                    Customer addCustomer = customerInfoInput(new Customer());
+                    allCustomers.add(addCustomer);
+                    allCustomers.refresh(allGroups);
+                    System.out.printf(
+                            "＞　Success !!! %s  ＜\n" +
+                                    "           .A__A    ✨\uD83C\uDF82✨    A__A\n" +
+                                    "           ( •⩊•)  _______ (•⩊• )\n" +
+                                    "           (>\uD83C\uDF70>)   |   |   (<\uD83D\uDD2A<)\n", addCustomer);
                 }
                 break;
             }catch (InputEndException e) {
@@ -58,11 +66,9 @@ public class CustomerMenu implements Menu {
         }
     }
 
-    private void customerInfoInput(int count) {
-        Customer addCustomer = new Customer();
+    private Customer customerInfoInput(Customer customer) {
         while(true) {
             try {
-                System.out.printf("=============== Customer %d Info. ===============\n", count);
                 int choice = chooseMenu(new String[]{
                         "Customer Name",
                         "Customer ID",
@@ -72,40 +78,30 @@ public class CustomerMenu implements Menu {
                 });
                 if(choice == 1) {
                     System.out.print("Input Customer Name : ");
-                    String customerName = nextLine(Message.END_MSG);
-                    addCustomer.setCusName(customerName);
+                    customer.setCusName(nextLine(Message.END_MSG));
                 }else if (choice == 2) {
                     System.out.print("Input Customer ID : ");
-                    String customerId = nextLine(Message.END_MSG);
-                    addCustomer.setCusId(customerId);
+                    customer.setCusId(nextLine(Message.END_MSG));
                 }else if(choice == 3) {
                     System.out.print("Input Customer Spent Time : ");
-                    int customerSpentTime = Integer.parseInt(nextLine(Message.END_MSG));
-                    addCustomer.setCusTotalTime(customerSpentTime);
+                    customer.setCusTotalTime(Integer.parseInt(nextLine(Message.END_MSG)));
                 }else if(choice == 4) {
                     System.out.print("Input Customer Total Pay : ");
-                    int customerTotalPay = Integer.parseInt(nextLine(Message.END_MSG));
-                    addCustomer.setCusTotalPay(customerTotalPay);
+                    customer.setCusTotalPay(Integer.parseInt(nextLine(Message.END_MSG)));
                 }else {
-                    if(addCustomer.getCusId() == null || addCustomer.getCusName() == null ||
-                            addCustomer.getCusTotalTime() == null || addCustomer.getCusTotalPay() == null) {
+                    if(customer.getCusId() == null || customer.getCusName() == null ||
+                            customer.getCusTotalTime() == null || customer.getCusTotalPay() == null) {
                         System.out.println(Message.ERR_MSG_INVALID_INPUT_NULL);
                     }else {
-                        allCustomers.add(addCustomer);
-                        allCustomers.refresh(allGroups);
-                        System.out.printf(
-                                "＞　Success !!! %s  ＜\n" +
-                                        "           .A__A    ✨\uD83C\uDF82✨    A__A\n" +
-                                        "           ( •⩊•)  _______ (•⩊• )\n" +
-                                        "           (>\uD83C\uDF70>)   |   |   (<\uD83D\uDD2A<)\n", addCustomer);
-                        break;
+                        return customer;
                     }
                 }
             }catch (InputEmptyException e){
                 System.out.println(Message.ERR_MSG_INVALID_INPUT_EMPTY);
             }catch (NumberFormatException e) {
                 System.out.println(Message.ERR_MSG_INVALID_INPUT_FORMAT);
-                System.out.println();
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(Message.ERR_MSG_INVALID_INPUT_RANGE);
             }
         }
     }
@@ -115,12 +111,14 @@ public class CustomerMenu implements Menu {
             System.out.println(Message.ERR_MSG_INVALID_ARR_EMPTY);
             System.out.println();
         }else {
-            System.out.println("======= Customer Info. =======");
-            for(int i = 0; i < allCustomers.size(); i++) {
+            System.out.println("======================================== Customer Info. ========================================");
+            int size = allCustomers.size();
+            for(int i = 0; i < size; i++) {
                 System.out.printf("No. %3d => ", i + 1 );
                 System.out.println(allCustomers.get(i));
                 System.out.println();
             }
+            System.out.println("==================================================================================================");
         }
     }
 
@@ -135,49 +133,12 @@ public class CustomerMenu implements Menu {
                     System.out.printf("Which customer ( 1 ~ %d ) ? : ", allCustomers.size());
                     int updateIndex = Integer.parseInt(nextLine(Message.END_MSG));
                     Customer updateCustomer = allCustomers.get(updateIndex - 1);
-                    while(true) {
-                        int choice = chooseMenu(new String[]{
-                                "Customer Name",
-                                "Customer ID",
-                                "Customer Spent Time",
-                                "Customer Total Pay",
-                                "Back"
-                        });
-                        if(choice == 1) {
-                            System.out.print("Input Customer Name : ");
-                            String customerName = nextLine();
-                            updateCustomer.setCusName(customerName);
-                        }else if (choice == 2) {
-                            System.out.print("Input Customer ID : ");
-                            String customerId = nextLine();
-                            updateCustomer.setCusId(customerId);
-                        }else if(choice == 3) {
-                            System.out.print("Input Customer Spent Time : ");
-                            int customerSpentTime = Integer.parseInt(nextLine());
-                            updateCustomer.setCusTotalTime(customerSpentTime);
-                        }else if(choice == 4) {
-                            System.out.print("Input Customer Total Pay : ");
-                            int customerTotalPay = Integer.parseInt(nextLine());
-                            updateCustomer.setCusTotalPay(customerTotalPay);
-                        }else {
-                            allCustomers.refresh(allGroups);
-                            System.out.printf(
-                                    "SUCCESS UPDATE!!\n" +
-                                    "%s\n" +
-                                    "두다다다다다다다\n" +
-                                    "　(∩`・ω・)\n" +
-                                    "＿/_ミつ/￣￣￣/\n" +
-                                    "　　＼/＿＿＿/\n", updateCustomer);
-                            break;
-                        }
-                    }
-                }catch (InputEndException e ) {
+                    customerInfoInput(updateCustomer);
+                    allCustomers.refresh(allGroups);
+                }catch (InputEndException e) {
                     System.out.println(Message.ERR_MSG_INPUT_END);
+                    System.out.println();
                     break;
-                }catch (NumberFormatException e){
-                    System.out.println(Message.ERR_MSG_INVALID_INPUT_FORMAT);
-                }catch (IndexOutOfBoundsException e) {
-                    System.out.println(Message.ERR_MSG_INVALID_INPUT_RANGE);
                 }
             }
         }
